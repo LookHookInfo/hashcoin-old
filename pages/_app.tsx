86d9e2c2450ac1a0be1087423f3172f62d06f148
look hook <admin@lookhook.info>
2024-05-10 09:29:41 +0200

    up

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,10 +5,7 @@ import { ChakraProvider } from "@chakra-ui/react";
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
 
-// This is the chain your dApp will work on.
-// Change this to the chain your app is built for.
-// You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-// const activeChain = "PolygonZkevmTestnet";
+
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
