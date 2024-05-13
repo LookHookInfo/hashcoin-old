4703cfef978c2e1d9b53fbda4bbac889fc975de3
look hook <admin@lookhook.info>
2024-05-13 16:11:45 +0200

    info

--- pages/info.tsx
+++ pages/info.tsx
@@ -32,7 +32,7 @@ export default function Info() {
                     <Heading as="h2" mb={4}>MemeHash Roadmap</Heading>
                     <Text>
                         - Development of the concept. <Icon as={MdCheckCircle} color="green.500" /><br />
-                        - Token issuance (explorer) limited supply.<br />
+                        - Token issuance ({""}<a href="https://basescan.org/address/0xa9b631abcc4fd0bc766d7c0c8fcbf866e2bb0445" target="_blank" rel="noopener noreferrer" style={{ color: 'blue' }}>Contract</a>) limited supply. <Icon as={MdCheckCircle} color="green.500" /><br />
                         - Founder&apos;s comment (Link).<br />
                         - Integration of DApp into EVM.<Icon as={MdCheckCircle} color="yellow.500" /><br />
                         - Formation of the EVM ecosystem.<Icon as={MdCheckCircle} color="yellow.500" /><br />
