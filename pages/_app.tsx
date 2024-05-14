5817501b43a0e02a00cec0a3e1212c3a756ebe85
look hook <admin@lookhook.info>
2024-05-14 12:51:05 +0200

    DAI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,13 @@
 import type { AppProps } from "next/app";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
-import { Sepolia } from "@thirdweb-dev/chains";
+import { Base } from "@thirdweb-dev/chains";
 import { ChakraProvider } from "@chakra-ui/react";
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThirdwebProvider activeChain={Sepolia}>
+    <ThirdwebProvider activeChain={Base}>
       <ChakraProvider>
         <NavBar />
         <div style={{ maxWidth: "95%", margin: "0 auto" }}>
