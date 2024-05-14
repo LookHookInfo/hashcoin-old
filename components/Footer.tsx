85fa1f148b4bd1d0f3879665a8edd5e7d841bb36
look hook <admin@lookhook.info>
2024-05-14 18:27:07 +0200

    up

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,55 +1,42 @@
 import React from 'react';
-import { Flex, Link } from "@chakra-ui/react";
+import { Flex, Link, Box } from "@chakra-ui/react";
 import { FaTwitter, FaDiscord, FaTelegram, FaGithub } from "react-icons/fa";
 
 const Footer: React.FC = () => {
     return (
-        <div style={{
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center",
-            padding: "1rem",
-            backgroundColor: "#01111f",
-            color: "#ffffff",
-            width: "100%", 
-            maxWidth: "95%", 
-            margin: "0 auto", 
-        }}>
+        <Flex justifyContent="space-between" alignItems="center" padding="1rem" backgroundColor="#01111f" color="#ffffff" width="100%" maxWidth="95%" margin="0 auto">
             <div>
                 <p>
                     DEV
-                    <Link
-                        href="https://lookhook.info/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        style={{ color: "#ffffff", textDecoration: "underline", marginLeft: "0.2rem" }}
-                    >
+                    <Link href="https://lookhook.info/" target="_blank" rel="noopener noreferrer" color="#ffffff" textDecoration="underline" marginLeft="0.2rem">
                         LookHook
                     </Link>
                 </p>
             </div>
-            <div>
-                <Flex justifyContent="center" alignItems="center">
-                    <Link href={"https://twitter.com/HashCoinFarm"} mx={2} target="_blank" rel="noopener noreferrer">
-                        <FaTwitter size={24} color="#ffffff" />
-                    </Link>
-                    <Link href={"https://discord.gg/D55sWhNgcb"} mx={2} target="_blank" rel="noopener noreferrer">
-                        <FaDiscord size={24} color="#ffffff" />
-                    </Link>
-                    <Link href={"https://t.me/ChainInside/524"} mx={2} target="_blank" rel="noopener noreferrer">
-                        <FaTelegram size={24} color="#ffffff" />
-                    </Link>
-                    <Link href={"https://github.com/LookHookInfo"} mx={2} target="_blank" rel="noopener noreferrer">
-                        <FaGithub size={24} color="#ffffff" />
-                    </Link>
-                </Flex>
-            </div>
-            <div>
+            <Flex justifyContent="center" alignItems="center">
+                <Link href="https://twitter.com/HashCoinFarm" mx={2} target="_blank" rel="noopener noreferrer">
+                    <FaTwitter size={24} color="#ffffff" />
+                </Link>
+                <Link href="https://discord.gg/D55sWhNgcb" mx={2} target="_blank" rel="noopener noreferrer">
+                    <FaDiscord size={24} color="#ffffff" />
+                </Link>
+                <Link href="https://t.me/ChainInside/524" mx={2} target="_blank" rel="noopener noreferrer">
+                    <FaTelegram size={24} color="#ffffff" />
+                </Link>
+                <Link href="https://github.com/LookHookInfo" mx={2} target="_blank" rel="noopener noreferrer">
+                    <FaGithub size={24} color="#ffffff" />
+                </Link>
+            </Flex>
+            <Box display={{ base: "none", md: "block" }}>
                 <a href="https://thirdweb.com" target="_blank" rel="noopener noreferrer" style={{ color: "white" }}>
                     ThirdWeb
                 </a>
-            </div>
-        </div>
+                <a>&nbsp;</a>
+                <a href="/eco" style={{ color: "white" }}>
+                    Eco
+                </a>
+            </Box>
+        </Flex>
     );
 };
 
---
+++ pages/eco.tsx
@@ -0,0 +1,44 @@
+import { Container, Heading, Text, Divider, Box, Icon } from "@chakra-ui/react";
+import { MdCheckCircle } from "react-icons/md";
+
+export default function Info() {
+    return (
+        <Container maxW="95%" mt={8} px={4} bg="gray.100">
+            <Box borderWidth="1px" borderRadius="lg" mb={5}>
+                <Box p={6}>
+                    <Heading as="h2" mb={4}>Partners # Hash</Heading>
+                    <Text fontSize="xl">Ecosystem partners</Text>
+                </Box>
+            </Box>
+
+            <Divider mb={5} />
+
+            <Box borderWidth="1px" borderRadius="lg" mb={5}>
+                <Box p={6}>
+                    <Heading as="h2" mb={4}>Aggregators</Heading>
+                    <Text mb={2}><b>DexTools</b> ({""}<a href="https://www.dextools.io/" target="_blank" rel="noopener noreferrer" style={{ color: 'blue' }}>Hash Coin</a>)</Text>
+                </Box>
+            </Box>
+
+            <Divider mb={5} />
+
+            <Box borderWidth="1px" borderRadius="lg" mb={5}>
+                <Box p={6}>
+                    <Heading as="h2" mb={4}>Media</Heading>
+                    <Text>
+                        - Media. <br />
+                    </Text>
+                </Box>
+            </Box>
+
+            <Divider mb={5} />
+
+            <Box borderWidth="1px" borderRadius="lg" mb={5}>
+                <Box p={6}>
+                    <Heading as="h2" mb={4}>DEX #hash</Heading>
+                    <Text>- DEX.</Text>
+                </Box>
+            </Box>
+        </Container>
+    );
+}
