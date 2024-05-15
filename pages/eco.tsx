a51d81b567148d6545c7ea7770cb550d7d501161
look hook <admin@lookhook.info>
2024-05-15 12:57:27 +0200

    up

--- pages/eco.tsx
+++ pages/eco.tsx
@@ -16,7 +16,7 @@ export default function Info() {
             <Box borderWidth="1px" borderRadius="lg" mb={5}>
                 <Box p={6}>
                     <Heading as="h2" mb={4}>Aggregators</Heading>
-                    <Text mb={2}><b>DexTools</b> ({""}<a href="https://www.dextools.io/app/en/base/pair-explorer/0x72cd910631a66149d3ce95d8c382de1e1d758096?t=1715768426824" target="_blank" rel="noopener noreferrer" style={{ color: 'blue' }}>Hash Coin</a>)</Text>
+                    <Text mb={2}><b>DexTools</b> ({""}<a href="https://www.dextools.io/app/en/base/pair-explorer/0x72cd910631a66149d3ce95d8c382de1e1d758096?t=1715768426824" target="_blank" rel="noopener noreferrer" style={{ color: 'blue' }}>Hash Coin</a>) 0x72cd910631a66149d3ce95d8c382de1e1d758096</Text>
                 </Box>
             </Box>
 
