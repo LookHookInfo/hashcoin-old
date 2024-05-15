ac5d8e50bc62ecd07bcb838ba938222ad8dbafa3
look hook <admin@lookhook.info>
2024-05-15 12:48:02 +0200

    logo

---
+++ pages/promo.tsx
@@ -0,0 +1,24 @@
+import { Container, Heading, Text, Divider, Box, Image } from "@chakra-ui/react";
+
+export default function Info() {
+    return (
+        <Container maxW="95%" mt={8} px={4} bg="gray.100">
+            <Box borderWidth="1px" borderRadius="lg" mb={5}>
+                <Box p={6}>
+                    <Heading as="h2" mb={4}>Promo # Hash</Heading>
+                    <Text fontSize="xl">Promo</Text>
+                </Box>
+            </Box>
+
+            <Divider mb={5} />
+
+            <Box borderWidth="1px" borderRadius="lg" mb={5}>
+                <Box p={6}>
+                    <Heading as="h2" mb={4}>Logo</Heading>
+                    <Text mb={2}><b>Logo 100x100</b></Text>
+                    <Image src="/img/logo100.png" alt="Hash Coin Logo" boxSize="100px" />
+                </Box>
+            </Box>
+        </Container>
+    );
+}
