c3a768de0e0c1e64be1d42a639a95940c15f2946
look hook <admin@lookhook.info>
2024-05-24 19:11:49 +0200

    paper

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,42 +1,61 @@
 import React from 'react';
-import { Flex, Link, Box } from "@chakra-ui/react";
-import { FaTwitter, FaDiscord, FaTelegram, FaGithub } from "react-icons/fa";
+import { Flex, Link, Box, Text, Divider, Icon } from "@chakra-ui/react";
+import { FaTwitter, FaDiscord, FaTelegram, FaGithub, FaExternalLinkAlt } from "react-icons/fa";
 
 const Footer: React.FC = () => {
     return (
-        <Flex justifyContent="space-between" alignItems="center" padding="1rem" backgroundColor="#01111f" color="#ffffff" width="100%" maxWidth="95%" margin="0 auto">
-            <div>
-                <p>
-                    DEV
-                    <Link href="https://lookhook.info/" target="_blank" rel="noopener noreferrer" color="#ffffff" textDecoration="underline" marginLeft="0.2rem">
-                        LookHook
-                    </Link>
-                </p>
-            </div>
-            <Flex justifyContent="center" alignItems="center">
+        <Box backgroundColor="#01111f" color="#ffffff" padding="1rem">
+            <Flex justifyContent="space-between" width="100%" maxWidth="95%" margin="0 auto" wrap="wrap">
+                {/* First Column */}
+                <Box flex="1" textAlign="center" padding="1rem">
+                    <Text><b>LookHook</b></Text>
+                    <Text>
+                        <Link href="https://lookhook.info" isExternal color="white">
+                            Team <Icon as={FaExternalLinkAlt} marginLeft="0.2rem" />
+                        </Link>
+                    </Text>
+                    <Text color="gray.400">Invest</Text>
+                </Box>
+
+                <Divider orientation="vertical" height="auto" borderColor="gray.600" display={{ base: "none", md: "block" }} />
+
+                {/* Second Column */}
+                <Box flex="1" textAlign="center" padding="1rem">
+                    <Link href="https://hashcoin.farm/coin" color="white">Coin</Link>
+                    <br />
+                    <Link href="https://hashcoin.farm/info" color="white">Road</Link>
+                    <br />
+                    <Link href="https://hashcoin.farm/paper" color="white">Paper</Link>
+                </Box>
+
+                <Divider orientation="vertical" height="auto" borderColor="gray.600" display={{ base: "none", md: "block" }} />
+
+                {/* Third Column */}
+                <Box flex="1" textAlign="center" padding="1rem">
+                    <Link href="https://hashcoin.farm/promo" color="white">Promo</Link>
+                    <br />
+                    <Link href="https://hashcoin.farm/eco" color="white">Eco</Link>
+                    <br />
+                    <Text color="gray.400">#Tag</Text>
+                </Box>
+            </Flex>
+
+            <Divider borderColor="gray.600" marginY="1rem" />
+
+            {/* Social Media Links */}
+            <Flex justifyContent="center">
                 <SocialLink href="https://twitter.com/HashCoinFarm" icon={<FaTwitter size={24} color="#ffffff" />} />
                 <SocialLink href="https://discord.gg/D55sWhNgcb" icon={<FaDiscord size={24} color="#ffffff" />} />
                 <SocialLink href="https://t.me/ChainInside/524" icon={<FaTelegram size={24} color="#ffffff" />} />
                 <SocialLink href="https://github.com/LookHookInfo" icon={<FaGithub size={24} color="#ffffff" />} />
             </Flex>
-            <Box display={{ base: "none", md: "block" }}>
-                <PageLink href="https://thirdweb.com" text="ThirdWeb" />
-                &nbsp;
-                <PageLink href="/eco" text="Eco" targetSelf />
-            </Box>
-        </Flex>
+        </Box>
     );
 };
 
 const SocialLink: React.FC<{ href: string; icon: JSX.Element }> = ({ href, icon }) => (
     <Link href={href} mx={2} target="_blank" rel="noopener noreferrer">
         {icon}
-    </Link>
-);
-
-const PageLink: React.FC<{ href: string; text: string; targetSelf?: boolean }> = ({ href, text, targetSelf }) => (
-    <Link href={href} style={{ color: "white" }} target={targetSelf ? "_self" : "_blank"} rel="noopener noreferrer">
-        {text}
     </Link>
 );
 
---
+++ pages/paper.tsx
@@ -0,0 +1,90 @@
+import { Container, Heading, Text, Box, Button } from "@chakra-ui/react";
+
+export default function HashCoinWhitepaper() {
+    return (
+        <Container maxW="95%" mt={8} px={4} bg="gray.100">
+            <Box borderWidth="1px" borderRadius="lg" mb={5} p={6}>
+                <Heading as="h2" mb={4} textAlign="center">Hash Coin Paper</Heading>
+                <Text fontSize="xl" textAlign="justify">
+                    Welcome to the Hash Coin ecosystem, where innovation and community engagement are the driving forces behind our mission. Our tokenomics and mechanics are designed to ensure stability, incentivize participation, and support the long-term growth of our project. This document outlines the core aspects of token distribution, NFT staking mechanics, and profit distribution.
+                </Text>
+
+                <Heading as="h3" mt={8} mb={4}>Contract Information</Heading>
+                <Text fontSize="lg" textAlign="justify">
+                    Participants interact with the following contracts:
+                    <br />
+                    - <b>NFT Contract</b>: 0x884AFB0f27C1C7f50D93e010598aaE6Ace3a66Dc
+                    <br />
+                    - <b>Inventory Contract</b>: 0x13CE10a3e09FA8000BA8A13fCbe8813f476584e7
+                    <br />
+                    - <b>Rewards Contract</b>: 0xA9B631ABcc4fd0bc766d7C0C8fCbf866e2bB0445
+                    <br />
+                    - <b>Staking Contract</b>: 0xBBc4f75874930EB4d8075FCB3f48af2535A8E848
+                </Text>
+
+                <Heading as="h3" mt={8} mb={4}>Token Distribution</Heading>
+                <Text fontSize="lg" textAlign="justify">
+                    Our token distribution strategy ensures a balanced and sustainable ecosystem:
+                    <br />
+                    - <b>80% for Mining</b>: The majority of our tokens are available for mining, allowing participants to actively engage with the ecosystem.
+                    <br />
+                    - <b>20% for Project Development</b>: A portion of the tokens is allocated to support the current development and improvement of our project.
+                </Text>
+                <Button colorScheme="blue" mt={4} onClick={() => window.open("https://hashcoin.farm/coin", "_blank")}>Tokenomics</Button>
+
+                <Heading as="h3" mt={8} mb={4}>NFT Staking Mechanics</Heading>
+                <Text fontSize="lg" textAlign="justify">
+                    Participants can acquire and stake NFTs to receive rewards in HASH tokens. Mining speed depends on the type and quantity of NFTs. We offer various types of NFTs:
+                    <br />
+                    - <b>NFT GPU</b>: Cost: $1.99 / Mining Speed: 0.042 #hash per hour
+                    <br />
+                    - <b>NFT Asic</b>: Cost: $9.50 / Mining Speed: 0.42 #hash per hour
+                    <br />
+                    - <b>NFT Farm</b>: Cost: $29.20 / Mining Speed: 2.42 #hash per hour
+                    <br />
+                    In the future, additional inventory such as Rig, Rack, Container will be introduced.
+                </Text>
+
+                <Heading as="h3" mt={8} mb={4}>Key Features</Heading>
+                <Text fontSize="lg" textAlign="justify">
+                    - <b>Universal Participation</b>: Anyone can participate in our ecosystem without regional restrictions.
+                    <br />
+                    - <b>Initiation</b>: To start mining HASH tokens, a participant must create an NFT-space (registration). This minting process is free except for gas fees and is performed once per participant account. Afterward, participants can purchase NFT inventory for HASH token mining through staking. Participants have the right to exit staking and claim rewards at any time, considering the token balance in the pool. Rewards are distributed by the contract (0xBBc4f75874930EB4d8075FCB3f48af2535A8E848) on a first-come, first-served basis.
+                    <br />
+                    - <b>Flexibility of Staking</b>: Participants can withdraw NFTs from staking and list them for sale on the OTC NFT market if they decide to cease token mining.
+                </Text>
+
+                <Heading as="h3" mt={8} mb={4}>Profit Distribution from NFT Sales</Heading>
+                <Text fontSize="lg" textAlign="justify">
+                    Profits from NFT sales are strategically allocated to ensure the stability and growth of the project:
+                    <br />
+                    - <b>50%</b> of the proceeds are directed to the HASH/stablecoin liquidity pool, ensuring stability and liquidity in trading pairs.
+                    <br />
+                    - <b>35%</b> forms reserves in the Treasury Fund: For future investments and project development.
+                    <br />
+                    - <b>15%</b> to the Team Fund: Provides funding for the team to continue product development and improvement.
+                </Text>
+
+                <Heading as="h3" mt={8} mb={4}>Future Development</Heading>
+                <Text fontSize="lg" textAlign="justify">
+                    We strive for continuous improvement and innovation. Our future plans include:
+                    <br />
+                    - <b>Enhanced Analytics</b>: Adding analytical data for a deeper understanding of the HASH coin ecosystem and movement.
+                    <br />
+                    - <b>Improved Interface</b>: Enhancing the user interface to improve the user experience.
+                    <br />
+                    - <b>Community Engagement and Partner Acquisition</b>.
+                </Text>
+                <Button colorScheme="blue" mt={4} onClick={() => window.open("https://hashcoin.farm/info", "_blank")}>Road Map</Button>
+
+                <Heading as="h3" mt={8} mb={4}>Conclusion</Heading>
+                <Text fontSize="lg" textAlign="justify">
+                    Hash Coin aims to create a reliable and engaging ecosystem. Our tokenomics and NFT staking mechanics are designed to ensure stability, incentivize participation, and support the long-term growth of our project. Join us in building a thriving community and a sustainable future.
+                    <br />
+                    Follow our social networks for additional information and updates.
+                </Text>
+
+            </Box>
+        </Container>
+    );
+}
