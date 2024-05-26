064b6fa40ad07dba6d1aa9c41038eaf5234328cd
look hook <admin@lookhook.info>
2024-05-26 15:11:27 +0200

    road

--- pages/road.tsx
+++ pages/road.tsx
@@ -1,13 +1,13 @@
-import { Container, Heading, Text, Divider, Box, Icon, Button, Link } from "@chakra-ui/react";
-import { FaTelegram } from "react-icons/fa";
+import { Container, Heading, Text, Divider, Box, Icon } from "@chakra-ui/react";
 import { MdCheckCircle } from "react-icons/md";
+import { FaTelegram } from 'react-icons/fa';
 
 export default function Info() {
     return (
         <Container maxW="95%" mt={8} px={4} bg="gray.100">
             <Box borderWidth="1px" borderRadius="lg" mb={5}>
                 <Box p={6}>
-                <Heading as="h2" mb={4}>MemeHash Roadmap</Heading>
+                    <Heading as="h2" mb={4}>MemeHash Roadmap</Heading>
                     
                     <Heading as="h4" mb={4} fontSize="lg">Phase 1: Conceptualization and Foundation</Heading>
                     <Text fontSize="lg">
@@ -53,31 +53,7 @@ export default function Info() {
 
             <Divider mb={5} />
 
-            <Box borderWidth="1px" borderRadius="lg" mb={5}>
-                <Box p={6}>
-                    <Heading as="h4" mb={4} fontSize="lg">Phase 4: Future Developments and Innovations (Under Discussion)</Heading>
-                    <Text fontSize="lg">
-                        <Icon as={MdCheckCircle} color="rgba(128, 128, 128, 0.5)" /> New Inventory for HASH Mining:<br />
-                        <Box ml={4}>
-                            • GPU Rig: Introducing a new inventory item for token mining.<br />
-                            • Asic Miner Rack: Adding advanced Asic miners for enhanced mining capabilities.<br />
-                            • Mining Container: Developing a mining container for large-scale operations.<br />
-                        </Box>
-                        <Icon as={FaTelegram} color="blue.500" /> Development of a Telegram Bot: Creating a Telegram bot for simplified access and interaction with the application.<br />
-                    </Text>
-                </Box>
-            </Box>
-
-            <Divider mb={5} />
-
-            <Box borderWidth="1px" borderRadius="lg" mb={5}>
-                <Box p={6}>
-                    <Heading as="h4" mb={4} fontSize="lg">Conclusion</Heading>
-                    <Text fontSize="lg">
-                        Our roadmap clearly and structurally outlines the path of MemeHash's development and growth. We strive to achieve each goal and continuously innovate to bring value to our community. Stay tuned for updates and join us on this journey!
-                    </Text>
-                </Box>
-            </Box>
+            
         </Container>
     );
 }
