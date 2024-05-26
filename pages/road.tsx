257963a8228c774bcf6a506ee8982481763aabe8
look hook <admin@lookhook.info>
2024-05-26 17:58:48 +0200

    road

--- pages/road.tsx
+++ pages/road.tsx
@@ -9,13 +9,14 @@ export default function Info() {
 
 <Box borderWidth="1px" borderRadius="lg" mb={5}>
     <Box p={6}>
-        <Heading as="h4" mb={4} fontSize="lg">Phase 2: Community Building and Early Engagement</Heading>
+        <Heading as="h4" mb={4} fontSize="lg">Phase 1: Conceptualization and Foundation</Heading>
         <Text fontSize="lg">
-            <Icon as={MdCheckCircle} color="yellow.500" /> Discord Roles: Defining and assigning roles within the Discord community.<br />
-            <Icon as={MdCheckCircle} color="rgba(128, 128, 128, 0.5)" /> ⚡️Inventory Drop for the Community: Distributing NFT GPUs among the initial project participants (100 units).<br />
-            <Icon as={MdCheckCircle} color="yellow.500" /> Integration with Aggregators: Listing MemeHash on CoinMarketCap (CMC), CoinGecko (CG), and other aggregators.<br />
-            <Icon as={MdCheckCircle} color="rgba(128, 128, 128, 0.5)" /> More useful information in the product interface.<br />
-        </Text>
+                        <Icon as={MdCheckCircle} color="green.500" /> Concept Development: Defining the core idea and vision of MemeHash.<br />
+                        <Icon as={MdCheckCircle} color="green.500" /> Token Issuance ({''}<a href="https://basescan.org/address/0xa9b631abcc4fd0bc766d7c0c8fcbf866e2bb0445" target="_blank" rel="noopener noreferrer" style={{ color: 'blue' }}>Contract</a>): Launching the HASH token with limited supply.<br />
+                        <Icon as={MdCheckCircle} color="green.500" /> Founder's Comment: ({''}<a href="https://x.com/PrimeBlocks/status/1790817564932677791" target="_blank" rel="noopener noreferrer" style={{ color: 'blue' }}>Link</a>).<br />
+                        <Icon as={MdCheckCircle} color="green.500" /> DApp Integration into EVM: Implementing the decentralized application into Ethereum Virtual Machine.<br />
+                        <Icon as={MdCheckCircle} color="yellow.500" /> Formation of the HASH Ecosystem: Creating and developing the ecosystem around the product.<br />
+                    </Text>
     </Box>
 </Box>
 
