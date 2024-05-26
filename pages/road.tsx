1afa5033f61e3042906821017e74942034f02f0e
look hook <admin@lookhook.info>
2024-05-26 15:23:34 +0200

    road

--- pages/road.tsx
+++ pages/road.tsx
@@ -6,7 +6,24 @@ export default function Info() {
     return (
         <Container maxW="95%" mt={8} px={4} bg="gray.100">
             <Box borderWidth="1px" borderRadius="lg" mb={5}>
-    
+                <Box p={6}>
+                    <Heading as="h2" mb={4}>MemeHash Roadmap</Heading>
+                </Box>
+            </Box>
+
+            <Divider mb={5} />
+
+            <Box borderWidth="1px" borderRadius="lg" mb={5}>
+                <Box p={6}>
+                    <Heading as="h4" mb={4} fontSize="lg">Phase 1: Conceptualization and Foundation</Heading>
+                    <Text fontSize="lg">
+                        <Icon as={MdCheckCircle} color="green.500" /> Concept Development: Defining the core idea and vision of MemeHash.<br />
+                        <Icon as={MdCheckCircle} color="green.500" /> Token Issuance ({''}<a href="https://basescan.org/address/0xa9b631abcc4fd0bc766d7c0c8fcbf866e2bb0445" target="_blank" rel="noopener noreferrer" style={{ color: 'blue' }}>Contract</a>): Launching the HASH token with limited supply.<br />
+                        <Icon as={MdCheckCircle} color="green.500" /> Founder's Comment: ({''}<a href="https://x.com/PrimeBlocks/status/1790817564932677791" target="_blank" rel="noopener noreferrer" style={{ color: 'blue' }}>Link</a>)<br />
+                        <Icon as={MdCheckCircle} color="green.500" /> DApp Integration into EVM: Implementing the decentralized application into Ethereum Virtual Machine.<br />
+                        <Icon as={MdCheckCircle} color="yellow.500" /> Formation of the HASH Ecosystem: Creating and developing the ecosystem around the product.<br />
+                    </Text>
+                </Box>
             </Box>
 
             <Divider mb={5} />
@@ -42,7 +59,31 @@ export default function Info() {
 
             <Divider mb={5} />
 
-            
+            <Box borderWidth="1px" borderRadius="lg" mb={5}>
+                <Box p={6}>
+                    <Heading as="h4" mb={4} fontSize="lg">Phase 4: Future Developments and Innovations (Under Discussion)</Heading>
+                    <Text fontSize="lg">
+                        <Icon as={MdCheckCircle} color="rgba(128, 128, 128, 0.5)" /> New Inventory for HASH Mining:<br />
+                        <Box ml={4}>
+                            • GPU Rig: Introducing a new inventory item for token mining.<br />
+                            • Asic Miner Rack: Adding advanced Asic miners for enhanced mining capabilities.<br />
+                            • Mining Container: Developing a mining container for large-scale operations.<br />
+                        </Box>
+                        <Icon as={FaTelegram} color="blue.500" /> Development of a Telegram Bot: Creating a Telegram bot for simplified access and interaction with the application.<br />
+                    </Text>
+                </Box>
+            </Box>
+
+            <Divider mb={5} />
+
+            <Box borderWidth="1px" borderRadius="lg" mb={5}>
+                <Box p={6}>
+                    <Heading as="h4" mb={4} fontSize="lg">Conclusion</Heading>
+                    <Text fontSize="lg">
+                        Our roadmap clearly and structurally outlines the path of MemeHash's development and growth. We strive to achieve each goal and continuously innovate to bring value to our community. Stay tuned for updates and join us on this journey!
+                    </Text>
+                </Box>
+            </Box>
         </Container>
     );
 }
