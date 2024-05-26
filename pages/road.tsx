1188bdfac0f68a47cf84668ab624a0b5e79df33d
look hook <admin@lookhook.info>
2024-05-26 18:07:15 +0200

    road

--- pages/road.tsx
+++ pages/road.tsx
@@ -1,24 +1,25 @@
-import { Container, Heading, Text, Divider, Box, Icon } from "@chakra-ui/react";
+import { Container, Heading, Text, Divider, Box, Icon, Link } from "@chakra-ui/react";
 import { MdCheckCircle } from "react-icons/md";
 import { FaTelegram } from 'react-icons/fa';
 
 export default function Info() {
     return (
         <Container maxW="95%" mt={8} px={4} bg="gray.100">
-            <Divider mb={5} />
-
-<Box borderWidth="1px" borderRadius="lg" mb={5}>
-    <Box p={6}>
-        <Heading as="h4" mb={4} fontSize="lg">Phase 1: Conceptualization and Foundation</Heading>
-        <Text fontSize="lg">
+            {/* Секция с дорожной картой MemeHash */}
+            <Box borderWidth="1px" borderRadius="lg" mb={5}>
+                <Box p={6}>
+                    <Heading as="h2" mb={4}>MemeHash Roadmap</Heading>
+                    
+                    <Heading as="h4" mb={4} fontSize="lg">Phase 1: Conceptualization and Foundation</Heading>
+                    <Text fontSize="lg">
                         <Icon as={MdCheckCircle} color="green.500" /> Concept Development: Defining the core idea and vision of MemeHash.<br />
-                        <Icon as={MdCheckCircle} color="green.500" /> Token Issuance ({''}<a href="https://basescan.org/address/0xa9b631abcc4fd0bc766d7c0c8fcbf866e2bb0445" target="_blank" rel="noopener noreferrer" style={{ color: 'blue' }}>Contract</a>): Launching the HASH token with limited supply.<br />
-                        <Icon as={MdCheckCircle} color="green.500" /> Founder's Comment: ({''}<a href="https://x.com/PrimeBlocks/status/1790817564932677791" target="_blank" rel="noopener noreferrer" style={{ color: 'blue' }}>Link</a>).<br />
+                        <Icon as={MdCheckCircle} color="green.500" /> Token Issuance (<Link href="https://basescan.org/address/0xa9b631abcc4fd0bc766d7c0c8fcbf866e2bb0445" target="_blank" rel="noopener noreferrer" color="blue.500">Contract</Link>): Launching the HASH token with limited supply.<br />
+                        <Icon as={MdCheckCircle} color="green.500" /> Founder&apos;s Comment: (<Link href="https://x.com/PrimeBlocks/status/1790817564932677791" target="_blank" rel="noopener noreferrer" color="blue.500">Link</Link>)<br />
                         <Icon as={MdCheckCircle} color="green.500" /> DApp Integration into EVM: Implementing the decentralized application into Ethereum Virtual Machine.<br />
                         <Icon as={MdCheckCircle} color="yellow.500" /> Formation of the HASH Ecosystem: Creating and developing the ecosystem around the product.<br />
                     </Text>
-    </Box>
-</Box>
+                </Box>
+            </Box>
 
             <Divider mb={5} />
 
@@ -64,6 +65,17 @@ export default function Info() {
                             • Mining Container: Developing a mining container for large-scale operations.<br />
                         </Box>
                         <Icon as={FaTelegram} color="blue.500" /> Development of a Telegram Bot: Creating a Telegram bot for simplified access and interaction with the application.<br />
+                    </Text>
+                </Box>
+            </Box>
+
+            <Divider mb={5} />
+
+            <Box borderWidth="1px" borderRadius="lg" mb={5}>
+                <Box p={6}>
+                    <Heading as="h4" mb={4} fontSize="lg">Conclusion</Heading>
+                    <Text fontSize="lg">
+                        Our roadmap clearly and structurally outlines the path of MemeHash&apos;s development and growth. We strive to achieve each goal and continuously innovate to bring value to our community. Stay tuned for updates and join us on this journey!
                     </Text>
                 </Box>
             </Box>
